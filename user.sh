3cce6c20fe0d2c29359a0737f0d7ec634ae2906a
Antima Jain <antimajain915@gmail.com>
2022-02-18 14:26:48 +0530

    Permission to add in a group

--- user.sh
+++ user.sh
@@ -11,18 +11,25 @@ echo "Enter Your Password:"
 sudo passwd $username
 
 #Adding User to a Group
-echo "The available groups are -- innogeeks, innodemo, Select one:"
-read group
+echo "Do you want to get added in a group:"
+read option
+if [[ $option == "Y" || $option == "y" ]];
+then
+    echo "The available groups are -- innogeeks, innodemo, Select one:"
+    read group
 
-case $group in 
-    "innogeeks" )
-        sudo usermod -a -G innogeeks $username
-        echo "You are added to innogeeks group " ;;
-    "innodemo")
-        sudo usermod -a -G innodemo $username
-        echo "You are added to innodemo group " ;;
-    * )
-        echo "Group not Available!! " ;;
+    case $group in 
+        "innogeeks" )
+            sudo usermod -a -G innogeeks $username
+            echo "You are added to innogeeks group " ;;
+        "innodemo")
+            sudo usermod -a -G innodemo $username
+            echo "You are added to innodemo group " ;;
+        * )
+            echo "Group not Available!! " ;;
 
-esac
-    
+    esac
+else
+    echo "Ok,we won't add you to any group"
+fi
+        
