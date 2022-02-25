152fd92fd3b62d53bcf341f75119955da01c2e92
Antima Jain <antimajain915@gmail.com>
2022-02-25 16:39:04 +0530

    Added triggers for all tables

--- Database/ecommerce_postgresql.sql
+++ Database/ecommerce_postgresql.sql
@@ -26,6 +26,23 @@ CREATE TABLE orders(
 	order_name VARCHAR(30) ,
 	order_change TIMESTAMP NOT NULL
  );
+
+-- Create a table for managing all the updation and deletion of employee table 
+ CREATE TABLE employee_invoices(
+	invoice_id SERIAL PRIMARY KEY,
+	e_id INT NOT NULL,
+	e_name VARCHAR(30),
+	role VARCHAR(30),
+	permissions VARCHAR(30)
+); 
+
+-- Create a table for managing all the updation and deletion of product table 
+CREATE TABLE product_invoices(
+	invoice_id SERIAL PRIMARY KEY,
+	p_id INT NOT NULL,
+	p_name VARCHAR(30),
+	category VARCHAR(30)
+);
  
 -- Insert Some random data for employee, product and orders table
 INSERT INTO employee(e_name, role, permissions) VALUES('Antima Jain','Super Admin','Execute'),
@@ -41,6 +58,7 @@ INSERT INTO orders(product_order_id,order_name,order_created) VALUES(2,'Nike Sho
 													                (4,'Jeans',now()),
 													                (5,'LipBalm',now());
 
+-- VIEWS
 -- Create a View for showing the Category of products using the order id
 CREATE OR REPLACE VIEW category_details
 AS 
@@ -52,6 +70,7 @@ ON product.p_id = orders.product_order_id
 -- Using the view
 select * from category_details
 
+-- TRIGGERS
 -- Trigger function for updation and deletion of the orders table records
 CREATE OR REPLACE FUNCTION fn_order_invoices()
 	RETURNS TRIGGER
@@ -83,8 +102,79 @@ EXECUTE PROCEDURE fn_order_invoices();
 update orders set order_name = 'Adidas Shoes' where order_id = 1
 delete from orders where order_id = 4
 
+-- Trigger function for updation and deletion of the employee table records
+CREATE OR REPLACE FUNCTION fn_employee_invoices()
+	RETURNS TRIGGER
+	LANGUAGE PLPGSQL
+	AS
+	$$
+		BEGIN
+			IF (TG_OP = 'UPDATE') THEN
+				IF NEW.e_name <> OLD.e_name OR 
+				   NEW.role <> OLD.role OR 
+				   NEW.permissions <> OLD.permissions THEN
+					
+					INSERT INTO employee_invoices(e_id,e_name,role,permissions)
+					VALUES(OLD.e_id,OLD.e_name,OLD.role,OLD.permissions);
+				
+				END IF;
+				RETURN NEW;
+			ELSEIF (TG_OP = 'DELETE') THEN
+				INSERT INTO employee_invoices(e_id,e_name,role,permissions)
+				VALUES(OLD.e_id,OLD.e_name,OLD.role,OLD.permissions);
+				RETURN OLD;
+			END IF;
+		END;
+	$$
+
+CREATE TRIGGER trigger_employee_invoices
+BEFORE UPDATE OR DELETE 
+ON employee
+FOR EACH ROW
+EXECUTE PROCEDURE fn_employee_invoices();
+
+-- Use the trigger by updating or deleting any row
+update employee set role = 'Co-Host' where e_id = 2
+delete from employee where e_id = 3
+
+-- Trigger function for updation and deletion of the product table records
+CREATE OR REPLACE FUNCTION fn_product_invoices()
+	RETURNS TRIGGER
+	LANGUAGE PLPGSQL
+	AS
+	$$
+		BEGIN
+			IF (TG_OP = 'UPDATE') THEN
+				IF NEW.p_name <> OLD.p_name OR 
+				   NEW.category <> OLD.category THEN
+				   
+					INSERT INTO product_invoices(p_id,p_name,category)
+					VALUES(OLD.p_id,OLD.p_name,OLD.category);
+					
+				END IF;
+				RETURN NEW;
+			ELSEIF (TG_OP = 'DELETE') THEN
+				INSERT INTO product_invoices(p_id,p_name,category)
+					VALUES(OLD.p_id,OLD.p_name,OLD.category);
+				RETURN OLD;
+			END IF;
+		END;
+	$$
+
+CREATE TRIGGER trigger_product_invoices
+BEFORE UPDATE OR DELETE 
+ON product
+FOR EACH ROW
+EXECUTE PROCEDURE fn_product_invoices();
+
+-- Use the trigger by updating or deleting any row
+update product set category = 'Jewellery' where p_id = 3
+delete from product where p_id = 5
+
 -- To see all the rows of the tables
 select * from employee
 select * from product
 select * from orders
 select * from order_invoices
+select * from employee_invoices
+select * from product_invoices
